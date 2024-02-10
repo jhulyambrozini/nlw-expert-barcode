4f9746974e219a016b72141b4162b3c91e28c704
jhuly ambrozini <jhulyamfebr@gmial.com>
2024-02-10 14:31:15 -0300

    feat: implementando controllers para tag creator

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,12 +1,16 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     def validate_and_create(self, http_request: HttpRequest):
+        tag_creator_controller = TagCreatorController()
+
         body = http_request.body
         product_code = body['product_code']
 
         # logica de regra de negocio
+        formatted_response = tag_creator_controller.create(product_code)
         print(product_code)
         # retorno http
-        return HttpResponse(status_code=200, body={'hello': 'word'})
+        return HttpResponse(status_code=200, body=formatted_response)
